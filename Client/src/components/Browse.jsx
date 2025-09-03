f8a0fdeda73b6116a3ae3c5b09e27a9e400289d8
Shreyastiwari30 <shreyas.tiwari30@gmail.com>
2025-09-03 18:00:39 +0530

    Fix hooks import and Browse component for Vercel build

--- Client/src/components/Browse.jsx
+++ Client/src/components/Browse.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 import Job from "./Job";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchedQuery } from "@/redux/jobSlice";
-import useGetAllJobs from "@/hooks/useGetAllJobs";
+import useGetAllJobs from "../hooks/useGetAlljobs";
 
 const Browse = () => {
   const dispatch = useDispatch();
